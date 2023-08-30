7382ac9b22cedd459c3db331b9780e8fa9f7798a
Anthonylooby1 <anthonylooby@yahoo.com>
2023-08-30 13:41:12 -0400

    first commit

---
+++ Controllers/RazorController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Razor.Controllers;
+
+public class RazorController : Controller
+{
+    [HttpGet("")]
+    public ViewResult Index()
+    {
+        return View();
+    }
+}
